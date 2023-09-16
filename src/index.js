8e0833f23e2c9474106d06daa82790402e03d6e0
injae-348 <ant0593@naver.com>
2023-09-16 16:13:52 +0900

    setting eslint&prettier

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const hello = 'hello';
+
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
